b3f7476751e76feae23012be91c27eaf0d5f535d
Fin860 <f.van.der.ligt@sintlucasedu.nl>
2024-10-04 15:58:05 +0200

    dryxdr

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class playerMovement : MonoBehaviour
 {
+    public int playerTriangle = 1;
+    private int triangleSpeed = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D) && playerTriangle == 1 && transform.position.x <= 3f) 
+        {
+            transform.position = transform.position + new Vector3(triangleSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A) && playerTriangle == 1 && transform.position.x <= 3f)
+        {
+            transform.position = transform.position + new Vector3(-triangleSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }

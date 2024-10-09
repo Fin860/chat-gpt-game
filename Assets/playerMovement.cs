8804a5a599879dc22b0247ec10cbbc9ee31c6d76
Fin860 <f.van.der.ligt@sintlucasedu.nl>
2024-10-09 16:52:18 +0200

    yep im taking my time to mutch really gotta start working more

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class playerMovement : MonoBehaviour
 {
-    public int playerTriangle = 1;
-    private int triangleSpeed = 3;
+    public int playerNumber = 1;
+    private int triangleSpeed = 7;
+    private int triangleJump = 8;
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +17,19 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D) && playerTriangle == 1 && transform.position.x <= 3f) 
+        if (Input.GetKey(KeyCode.RightArrow) && playerNumber == 1) 
         {
-            transform.position = transform.position + new Vector3(triangleSpeed * Time.deltaTime, 0, 0);
+            transform.position = transform.position + new Vector3(triangleSpeed * Time.deltaTime, 0);
         }
 
-        if (Input.GetKey(KeyCode.A) && playerTriangle == 1 && transform.position.x <= 3f)
+        if (Input.GetKey(KeyCode.LeftArrow) && playerNumber == 1)
         {
-            transform.position = transform.position + new Vector3(-triangleSpeed * Time.deltaTime, 0, 0);
+            transform.position = transform.position + new Vector3(-triangleSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && playerNumber == 1)
+        {
+            transform.position = transform.position + new Vector3(0, triangleJump * Time.deltaTime);
         }
     }
 }
